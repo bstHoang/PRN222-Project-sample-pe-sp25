c0a2d7c622b02cd115b23b6872b644118191aee9
bstHoang <10t1qx4thaihoang@gmail.com>
2025-09-06 13:59:30 +0700

    q1 sample

---
+++ PE_PRN222_GivenSolution1/Project11/Program.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Project11.Models;
+using System.Net;
+using System.Text.Json;
+
+var builder = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false);
+
+var config = builder.Build();
+
+var optionsBuilder = new DbContextOptionsBuilder<LibraryContext>();
+optionsBuilder.UseSqlServer(config.GetConnectionString("LibraryDb"));
+
+var listener = new HttpListener();
+listener.Prefixes.Add("http://localhost:8080/");
+listener.Start();
+Console.WriteLine("Server started at http://localhost:8080/");
+
+while (true)
+{
+    var context = await listener.GetContextAsync();
+    var response = context.Response;
+
+    if (context.Request.Url.AbsolutePath == "/books")
+    {
+        using var db = new LibraryContext(optionsBuilder.Options);
+        var books = db.Books.ToList();
+
+        var json = JsonSerializer.Serialize(books);
+        var buffer = System.Text.Encoding.UTF8.GetBytes(json);
+        response.ContentType = "application/json";
+        response.ContentLength64 = buffer.Length;
+
+        await response.OutputStream.WriteAsync(buffer, 0, buffer.Length);
+    }
+    else
+    {
+        response.StatusCode = 404;
+    }
+
+    response.Close();
+}
